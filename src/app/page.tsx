1d2463bfb17c97dc643e4b5f36e766e6013b9308
izmktr <izmktr@gmail.com>
2025-10-27 15:24:50 +0900

    中国剰余定理のソルバーを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
   const Solver = [
     ["しりとりソルバー", "/shiritori"],
     ["スケルトンソルバー", "/skeleton"],
+    ["中国剰余定理", "/chinese-remainder"],
     ["覆面算ソルバー", "/cryptarithmetic"],
   ];
 
